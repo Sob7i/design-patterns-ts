808468e2b88953d96d63d1a4dd9075c5eafadf2e
Sobhi <sub7i.alkhuder@gmail.com>
2021-10-12 14:47:05 +0200

    Refactoring

--- Behavioral-patterns/Iterator/generator.ts
+++ Behavioral-patterns/Iterator/generator.ts
@@ -2,7 +2,7 @@ import fetch from 'node-fetch'
 
 async function* iterateApiPages<DataType>(
   url: string,
-): AsyncGenerator<DataType, void, undefined> {
+): AsyncGenerator<DataType, unknown, undefined> {
   let nextUrl: string | null | undefined = url
 
   do {
@@ -10,7 +10,7 @@ async function* iterateApiPages<DataType>(
     const json: {
       next?: string | null
       results: DataType[]
-    } = await response.json()
+    } | any = await response.json()
 
     yield* json.results
 
@@ -25,7 +25,7 @@ interface Pokemon {
   url: string
 }
 
-;(async function (): Promise<void> {
+(async function (): Promise<void> {
   /* Get each result at a time */
   const results = iterateApiPages<Pokemon>('https://pokeapi.co/api/v2/pokemon/')
 
@@ -34,7 +34,6 @@ interface Pokemon {
   
   console.log(`nextResult`, nextResult)
   console.log(`isDone`, isDone)
-
 
   /* Or iterate through the results */
   for await (const result of iterateApiPages<Pokemon>(
